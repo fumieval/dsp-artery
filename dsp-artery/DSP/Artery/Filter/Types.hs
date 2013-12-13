a6047ab484bbd33fc37cfdca119731d93158e0e0
Fumiaki Kinoshita <fumiexcel@gmail.com>
2013-12-13 22:47:54 +0900

    strengthen

--- dsp-artery/DSP/Artery/Filter/Types.hs
+++ dsp-artery/DSP/Artery/Filter/Types.hs
@@ -1,6 +1,6 @@
 module DSP.Artery.Filter.Types (FilterParam(..)) where
 
 data FilterParam a = FilterParam
-    { _filterCutOff :: a
-    , _filterQ :: a
+    { _filterCutOff :: !a
+    , _filterQ :: !a
     }
